d86774df491bc4ad67a53e7b6304d4888a447fa2
Ana Payán Ruiz <56390933+anapayanruiz@users.noreply.github.com>
2026-03-17 18:53:21 +0000

    feat: implement useMemo to memoize full name and avoid unnecessary recalculations

---
+++ ejemplos-ana/reactjs-hooks-usememo-lab/src/App.tsx
@@ -0,0 +1,14 @@
+import { Component } from "react";
+import Persona from "./components/Persona";
+
+class App extends Component {
+  render() {
+    return (
+      <div>
+        <Persona />
+      </div>
+    );
+  }
+}
+
+export default App;
---
+++ ejemplos-ana/reactjs-hooks-usememo-lab/src/components/Persona.tsx
@@ -0,0 +1,41 @@
+import { useState, useMemo } from "react";
+
+const Persona = () => {
+  const [nombre, setNombre] = useState("Charly");
+  const [apellido, setApellido] = useState("Falco");
+  const [email, setEmail] = useState("cfalco@gmail.com");
+
+  const nombreCompleto = useMemo(() => {
+    console.log("nombreCompleto", nombre, apellido);
+    return (
+      <p>
+        {nombre} {apellido}
+      </p>
+    );
+  }, [nombre, apellido]);
+
+  return (
+    <div>
+      <input
+        type="text"
+        value={nombre}
+        onChange={(e) => setNombre(e.target.value)}
+      />
+      <input
+        type="text"
+        value={apellido}
+        onChange={(e) => setApellido(e.target.value)}
+      />
+      <input
+        type="text"
+        value={email}
+        onChange={(e) => setEmail(e.target.value)}
+      />
+      <hr />
+      <p>{nombreCompleto}</p>
+      <p>{email}</p>
+    </div>
+  );
+};
+
+export default Persona;
